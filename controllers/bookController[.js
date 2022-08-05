5e561475d974fc1704c7734ae1db1b714530e5f2
André <andremessias.az@gmail.com>
2022-08-05 19:05:50 -0300

    update: add index controller to bookController

--- controllers/bookController[.js
+++ controllers/bookController[.js
@@ -1,5 +1,9 @@
 const Book = require("../models/book");
 
+exports.index = function (req, res) {
+  res.send("NOT IMPLEMENTED: Site home page");
+}
+
 // Display list of all books.
 exports.book_list = function (req, res) {
   res.send("NOT IMPLEMENTED YET: book list");
