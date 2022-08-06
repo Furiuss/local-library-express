2bfbd8d134ef37f710c7ffd0ab2dbad4e05a063f
André <andremessias.az@gmail.com>
2022-08-06 15:11:18 -0300

    fix: solve some bugs

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -48,13 +48,12 @@ exports.author_create_post = [
       res.render("author_form", {
         title: "Create Author",
         author: req.body,
-        errors: errors.arrays(),
+        errors: errors.array(),
       });
-      return;
-    } else {
+      return;    } else {
       const author = new Author({
         first_name: req.body.first_name,
-        last_name: req.body.last_name,
+        family_name: req.body.family_name,
         date_of_birth: req.body.date_of_birth,
         date_of_death: req.body.date_of_death,
       });
