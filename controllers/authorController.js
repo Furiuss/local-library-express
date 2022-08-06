a74ec0651cc8cb53d43134caf80aa3e578e896fe
André <andremessias.az@gmail.com>
2022-08-06 14:59:23 -0300

    setup: add validation to setup author_create controller

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,5 @@
 const Author = require("../models/author");
+const { body, validationResult } = require("express-validator");
 
 // Display list of all Authors.
 exports.author_list = function (req, res) {
@@ -12,13 +13,59 @@ exports.author_detail = function (req, res) {
 
 // Display Author create form on GET
 exports.author_create_get = function (req, res) {
-  res.send("NOT IMPLEMENTED YET: Author create GET");
+  res.render("author_form", { title: "Create Author" });
 };
 
 // Handle Author create POST
-exports.author_create_post = function (req, res) {
-  res.send("NOT IMPLEMENTED YET: Author create POST");
-};
+exports.author_create_post = [
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters"),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render("author_form", {
+        title: "Create Author",
+        author: req.body,
+        errors: errors.arrays(),
+      });
+      return;
+    } else {
+      const author = new Author({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death,
+      });
+      author.save((err) => {
+        if (err) return next(err);
+
+        res.redirect(author.url);
+      });
+    }
+  },
+];
 
 // Display Author delete form on GET
 exports.author_delete_get = function (req, res) {
@@ -37,5 +84,5 @@ exports.author_update_get = function (req, res) {
 
 // Handle Author delete on POST
 exports.author_update_post = function (req, res) {
-  res.send('NOT IMPLEMENTED YET: Author update POST')
-}
+  res.send("NOT IMPLEMENTED YET: Author update POST");
+};
